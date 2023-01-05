fb280688f381fa3a7f7ee22d36c023fc19275109
choopop <thy0217@naver.com>
2023-01-05 19:17:58 +0900

    2023-01-05 인덱스js충돌부분 수기로 수정후 메인브랜치 푸시

--- KODUP_ADMIN/src/main/resources/static/js/index.js
+++ KODUP_ADMIN/src/main/resources/static/js/index.js
@@ -1,14 +1,6 @@
 /**
  * 
  */
-<<<<<<< HEAD
- 
-  $('.btnChartBoard').on('click',function(){
-  console.log("연결");
- 	$('#baik_admin_main').load('jsp/admin_chart_board');
- });
- 
-=======
 
 //index.js 공통작업파일은 수작업으로 합치도록함.
 //커밋푸시할때 index.js는 빼고 푸시해서 메인브랜치로 머지.
@@ -16,36 +8,18 @@ $('.btnChartBoard').on('click',function(){
 	console.log("index.js admin_chart_board.jsp 연결성공");
 	$('#baik_admin_main').load('jsp/admin_chart_board');
 });
->>>>>>> refs/remotes/origin/seongho
 
-<<<<<<< HEAD
-/*
-  $('.btnChartMember').on('click',function(){
-  console.log("연결");
- 	$('#baik_admin_main').load('jsp/admin_chart_board');
- });
-=======
 
 $('.btnChartMember').on('click',function(){
 	console.log("index.js admin_chart_member.jsp 연결성공");
 	$('#baik_admin_main').load('jsp/admin_chart_member');
 });
->>>>>>> refs/remotes/origin/seongho
- 
-<<<<<<< HEAD
-   $('.btnChartUse').on('click',function(){
-  console.log("연결");
- 	$('#btnChartMember').load('jsp/admin_chart_board');
- });
- 
- */
-=======
+
  
 $('.btnChartUse').on('click',function(){
 	console.log("index.js admin_chart_pixel.jsp 연결성공");
 	$('#baik_admin_main').load('jsp/admin_chart_pixel');
 });
-
 
 $('.btnPixelExchange').on('click',function(){
 	console.log("jsp/admin_exchange.jsp 연결성공");
@@ -60,5 +34,37 @@ $('.btnExchangeRate').on('click',function(){
 
 
 
+/*-------------------------------------------------------*/
+ $('.btnHome').on('click',function(){
+  console.log("연결");
+    $('#baik_admin_main').load('jsp/admin_total_board');
+ });
+ 
+ $('.btnTotalBoard').on('click',function(){
+  console.log("연결");
+    $('#baik_admin_main').load('jsp/admin_total_board');
+ });
 
->>>>>>> refs/remotes/origin/seongho
+$('.btnMemberList').on('click',function(){
+  console.log("연결");
+    $('#baik_admin_main').load('jsp/admin_member_list');
+ });
+
+$('.btnMemberBan').on('click',function(){
+  console.log("연결");
+    $('#baik_admin_main').load('jsp/admin_total_board');
+ });
+ 
+ $('.btnCorpCertification').on('click',function(){
+  console.log("연결");
+    $('#baik_admin_main').load('jsp/admin_corp_certification');
+ });
+ 
+  $('.btnMentoCertification').on('click',function(){
+  console.log("연결");
+    $('#baik_admin_main').load('jsp/admin_mento_certification');
+ });
+
+
+
+
